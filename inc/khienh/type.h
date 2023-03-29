7c65e667a1bd9e300d3e89bfda58447c4ea646d8
zoanana990 <harry021633@gmail.com>
2023-03-29 19:17:47 +0800

    add put char

---
+++ inc/khienh/type.h
@@ -0,0 +1,16 @@
+#ifndef TYPE_H
+#define TYPE_H
+
+/* unsigned variable type */
+typedef unsigned long long u64;
+typedef unsigned int       u32;
+typedef unsigned short     u16;
+typedef unsigned char      u8;
+
+/* signed variable */
+typedef long long   i64;
+typedef int         i32;
+typedef short       i16;
+typedef char        c8;
+
+#endif
--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,14 @@
-#include "kernel.h"
-#include <stddef.h>
-#include <stdint.h>
+#include <khienh/kernel.h>
+#include <khienh/type.h>
 
+/* put character, and we can assign the color for the character */
+u16 put_char(c8 c, c8 color)
+{
+    return (color << 8) | c;
+}
 
 void kernel_main()
 {
-
+    u16 *video_mem = (u16*)(0xB8000);
+    video_mem[0] = put_char('A', 3);
 }
